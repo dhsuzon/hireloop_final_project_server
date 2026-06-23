db71649f3f67eb67f058853e4712b87132b88ae5
Mohammad Anwar <anwar.hussen.pro@gmail.com>
2026-06-23 12:35:17 +0600

    admin company approved and rejected logic route setup

--- controllers/companyControllers.js
+++ controllers/companyControllers.js
@@ -1,10 +1,21 @@
 const { companies, userCollection } = require("../config/db");
 
-//  create a new company
+const { ObjectId } = require("mongodb");
+
 const createNewCompany = async (req, res) => {
   try {
-    const companyInfo = req.body;
+    const { _id, ...companyInfo } = req.body;
 
+    // Check if we are updating an existing company
+    if (_id) {
+      const result = await companies.updateOne(
+        { _id: new ObjectId(_id) },
+        { $set: { ...companyInfo, updateAt: new Date() } },
+        { upsert: true },
+      );
+      return res.status(200).json(result);
+    }
+    //  create  a new company
     const newCompanyInfo = {
       ...companyInfo,
       createAt: new Date(),
@@ -33,14 +44,32 @@ const getDbUserOrRecruiterFakeAPI = async (req, res) => {
   const result = await userCollection.find({}).skip(1).toArray();
   res.status(200).json(result);
 };
-
-const getDbRecruiterAllCompanyFakeAPI = async (req, res) => {
+//  get the all company
+const getRecruiterAllCompany = async (req, res) => {
   const result = await companies.find({}).toArray();
   res.status(200).json(result);
+}; //  update company status
+const updateCompanyStatus = async (req, res) => {
+  const id = new ObjectId(req.params.id);
+  updateInfo = req.body;
+  const filter = { _id: id };
+  const updateCompanyDoc = {
+    $set: {
+      status: updateInfo.status,
+    },
+  };
+  try {
+    const updateResult = await companies.updateOne(filter, updateCompanyDoc);
+    res.status(200).json(updateResult);
+  } catch (error) {
+    res.status(500).json({ message: `internal server error ${error.message}` });
+  }
 };
+
 module.exports = {
   createNewCompany,
   getRecruiterComapany,
   getDbUserOrRecruiterFakeAPI,
-  getDbRecruiterAllCompanyFakeAPI,
+  getRecruiterAllCompany,
+  updateCompanyStatus,
 };
--- routes/companies.js
+++ routes/companies.js
@@ -4,15 +4,17 @@ const {
   createNewCompany,
   getRecruiterComapany,
   getDbUserOrRecruiterFakeAPI,
-  getDbRecruiterAllCompanyFakeAPI,
+  getRecruiterAllCompany,
+  updateCompanyStatus,
 } = require("../controllers/companyControllers");
 
 const router = Express.Router();
 
 router.get("/user", getDbUserOrRecruiterFakeAPI);
-router.get("/companies", getDbRecruiterAllCompanyFakeAPI);
+router.get("/all", getRecruiterAllCompany);
 
 router.get("/", getRecruiterComapany);
 router.post("/", createNewCompany);
+router.patch("/:id", updateCompanyStatus);
 
 module.exports = router;
